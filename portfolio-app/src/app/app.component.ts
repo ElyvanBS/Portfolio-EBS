709266b385d34be59790aab6002064895b44bf8b
ElyvanBS <elyvanbs@gmail.com>
2026-02-05 20:11:55 -0500

    Adiciona componente de contato e atualiza layout do portfólio

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { SobreComponent } from "./component/sobre/sobre.component";
 import { FormacaoComponent } from "./component/formacao/formacao.component";
 import { ProjetoComponent } from "./component/projeto/projeto.component";
+import { ContatoComponent } from "./component/contato/contato.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SobreComponent, FormacaoComponent, ProjetoComponent],
+  imports: [RouterOutlet, SobreComponent, FormacaoComponent, ProjetoComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
