7dd575cee5f7e58e2686b2920c724b3ec70dff55
ElyvanBS <elyvanbs@gmail.com>
2026-02-09 14:55:19 -0500

    Adiciona animação ao botão de retorno e ajusta a visibilidade com base na rolagem da página

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SobreComponent } from "./component/sobre/sobre.component";
 import { FormacaoComponent } from "./component/formacao/formacao.component";
 import { ProjetoComponent } from "./component/projeto/projeto.component";
 import { ContatoComponent } from "./component/contato/contato.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SobreComponent, FormacaoComponent, ProjetoComponent, ContatoComponent],
+  imports: [CommonModule, RouterOutlet, SobreComponent, FormacaoComponent, ProjetoComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Portfolio';
+
+   mostrarSup = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+    this.mostrarSup = scrollY > 200;
+  }
 }
